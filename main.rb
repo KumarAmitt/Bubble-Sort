6414394bbc2fbc5dffc84d1a4c5bd95d06f8a2c6
KumarAmitt <amitt2015@outlook.com>
2021-01-05 17:59:47 +0530

    Refactor methods

--- main.rb
+++ main.rb
@@ -1,23 +1,24 @@
-def bubble_sort (array)
-  loop do
-    swapped = false
-    (array.length - 1).times do |i|
-      if array[i] > array[i + 1]
-        array[i], array[i + 1] = array[i + 1], array[i]
-        swapped = true     
-      end
-    end
-    break unless swapped
-  end
-  array
-end
+# Swap Method---------------------------
 
 def swap(array, i)
   array[i], array[i+1] = array[i+1], array[i]
   true
 end
 
+# bubble_sort Method-----------------------
 
+def bubble_sort (array)
+  loop do
+    swapped = false
+    (array.length - 1).times do |i|
+      swapped = swap(array, i)  if array[i] > array[i + 1]
+    end
+    break unless swapped
+  end
+  array
+end
+
+# bubble_sort_by Method----------------------
 def bubble_sort_by(array)
   loop do
     swapped = false
@@ -29,8 +30,12 @@ def bubble_sort_by(array)
   array
 end
 
-# puts bubble_sort ([1,6,4,3,6,9])
+# For bubble_sort
+puts bubble_sort ([1,6,4,3,6,9])
 
+puts '-------------'
+
+# For bubble_sort_by
 puts bubble_sort_by ([1,6,4,3,6,9]) {|left, right| left <=> right}
 
-
+puts bubble_sort_by(["hi","hello","hey"]) { |left, right| left.length <=> right.length }
